c57ff2405f87afb33c4600ea08667be689e589eb
Matheus Alves Pereira <matttalves@gmail.com>
2022-08-22 13:47:21 -0300

    feat: component with props and emit event

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <img alt="Vue logo" src="./assets/logo.png" />
+    </div>
+    <div>
+      <button @click="toggleComponent">Change Component</button>
+    </div>
+
+    <component
+      :is="selectedComponent"
+      msg="Welcome to Your Vue.js App"
+      value="oi"
+      @show-message="showMessageTemporarily"
+    />
+
+
+    <div v-if="showMessage">
+      <p style="color: red">warning message</p>
+    </div>
+
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import FormField from './components/FormField.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    FormField,
+  },
+  data() {
+    return {
+      selectedComponent: 'HelloWorld',
+      showMessage: false
+    }
+  },
+
+  methods: {
+    toggleComponent () {
+      this.selectedComponent = this.selectedComponent === 'FormField' ? 'HelloWorld' : 'FormField'
+    },
+
+    showMessageTemporarily() {
+      console.log('message')
+      this.showMessage = true
+
+      setTimeout(() => {
+        this.showMessage = false
+      }, 1000)
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background: #333;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #FFF;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/FormField.vue
@@ -0,0 +1,35 @@
+<template>
+  <form class="form" @submit.prevent="emitEvent">
+    
+    <p>
+      <label for="message">{{ value }}</label>
+    </p>
+    
+    <input 
+      type="text"
+      id="message"
+      :value="value"
+    />
+
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'FormField',
+  props: {
+    value: {
+      type: String,
+      default: 'default'
+    }
+  },
+
+  methods: {
+    emitEvent() {
+      console.log('emitting event...')
+      this.$emit('show-message')
+    }
+  }
+}
+</script>
